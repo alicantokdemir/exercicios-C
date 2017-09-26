31740bcff85d054c1031e2d37172580b220abe97
Alican Tokdemir <alicantokdemir@gmail.com>
2017-09-26 12:39:20 -0300

    lista 3 exercicio 6

---
+++ Lista-3/6.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * A amplitude de uma relação de números reais é a diferença entre o maior e o menor valor da relação. Por exemplo, a amplitude de 5, 7, 15, 2, 23, 21, 3, 6 é 23-2=21. Elabore um programa em C recebe uma relação de números e forneça a sua amplitude.
+*/
+
+int main()
+{
+    int vetorTmn = 10;
+    int meuVetor[vetorTmn], i, oMaior, oMenor;
+
+    for (i = 0; i < vetorTmn; i++)
+    {
+        printf("Vetor[%d] valor: ", i);
+        scanf("%d", &meuVetor[i]);
+        if (!oMaior || oMaior < meuVetor[i])
+        {
+            oMaior = meuVetor[i];
+        }
+
+        if (!oMenor || oMenor > meuVetor[i])
+        {
+            oMenor = meuVetor[i];
+        }
+    }
+
+    printf("\nA amplitude do relacao e %d\n", oMaior - oMenor);
+
+    return 0;
+}
